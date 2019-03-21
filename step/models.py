6abc1d3fe26f5c2e1f78dc23fdcb71280f9351ab
bkarlovitz <bryan.a.karlovitz@gmail.com>
2019-03-21 15:56:10 -0400

    start models for pp participants

--- step/models.py
+++ step/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Gender(model.Model):
+    gender_value = model.CharField(max_length=30)
+
+class Race(model.Model):
+    race_value = model.CharField(max_length=30)
+
+# TODO: seed these tables ^^^
+
+class Participant(model.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    last_four_ssn = models.CharField(max_length=4)
+    pp_id = models.CharField(max_length=200)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
