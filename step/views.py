9074375488b0ddcccbd49a097ffa8861db4c6230
Bryan Karlovitz <bryan@alluresecurity.com>
2019-03-24 15:26:52 -0400

    restframework bootstrapped, test endpoint working

--- step/views.py
+++ step/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 def index(request):
     return HttpResponse("Hello, Prevention Point.")
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message', 'Hello, Prevention Point (from rest framework)'}
+        return Response(content)
