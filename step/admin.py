3a7524e4e32a67919d0dd47dae837a45e5ff6027
Thomas Vendetta <thomas@vendetta.io>
2019-03-25 00:08:57 -0400

    Add enums for some places and fix employee column clash

--- step/admin.py
+++ step/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Participant, Medication, UrineDrugScreen, CaseManagement, Appointment, Employee
 
 # Register your models here.
+admin.site.register(Participant)
+admin.site.register(Medication)
+admin.site.register(UrineDrugScreen)
+admin.site.register(CaseManagement)
+admin.site.register(Appointment)
+admin.site.register(Employee)
