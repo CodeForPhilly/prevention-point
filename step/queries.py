aaaf98207a26c618ec6f9801320b05e1303c6dcc
Bryan Karlovitz <bryan@alluresecurity.com>
2019-03-26 17:58:10 -0400

    add queries.py file, blank methods for employee crud

---
+++ step/queries.py
@@ -0,0 +1,18 @@
+def create_employee():
+    # TODO: implement method
+
+
+def find_employee():
+    # TODO: implement method
+
+
+def list_employees():
+    # TODO: implement method
+
+
+def update_employee():
+    # TODO: implement method
+
+
+def delete_employee():
+    # TODO: implement method
