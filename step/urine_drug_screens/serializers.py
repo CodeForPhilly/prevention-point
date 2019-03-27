d3eb3d1139578a1cbe98dbbd2562295c382dabd8
Thomas Vendetta <thomas@vendetta.io>
2019-03-27 18:41:44 -0400

    Add participant_id/id to uds serializer

--- step/urine_drug_screens/serializers.py
+++ step/urine_drug_screens/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UrineDrugScreenSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UrineDrugScreen
-        fields = ('date_of_test', 'uds_temp', 'pregnancy_test', 'opiates', 'fentanyl', 'bup', 'coc', 'amp', 'm_amp', 'thc', 'mtd', 'pcp', 'bar', 'bzo', 'tca', 'oxy')
+        fields = ('id', 'participant_id', 'date_of_test', 'uds_temp', 'pregnancy_test', 'opiates', 'fentanyl', 'bup', 'coc', 'amp', 'm_amp', 'thc', 'mtd', 'pcp', 'bar', 'bzo', 'tca', 'oxy')
