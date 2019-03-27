d7a604a1c7ca6167aae99f7f3600d7b6bcefb71b
Thomas Vendetta <thomas@vendetta.io>
2019-03-27 18:41:12 -0400

    Add participants API endpoint

---
+++ step/participants/serializers.py
@@ -0,0 +1,8 @@
+from step.models import Participant
+from rest_framework import serializers
+
+class ParticipantSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Participant
+
+        fields = ('id', 'first_name', 'last_name', 'last_four_ssn', 'pp_id', 'gender', 'race', 'date_of_birth', 'start_date')
