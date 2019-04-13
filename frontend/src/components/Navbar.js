d71480ddbc6c311a8c115f3848d845c8ff4cbfa3
grahamalama <graham.beckley@gmail.com>
2019-04-13 14:52:02 -0400

    A little navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,11 +4,14 @@ import { AppBar, Toolbar, Button } from "@material-ui/core"
 import { withRouter } from "react-router-dom"
 import fakeAuth from "../auth"
 
+import "../scss/navbar.scss"
+
 const SignOut = withRouter(
   ({ history }) =>
     fakeAuth.isAuthenticated && (
       <Button
         variant="contained"
+        className="signOut"
         color="secondary"
         onClick={() => {
           fakeAuth.signout(() => history.push("/"))
@@ -22,7 +25,7 @@ const SignOut = withRouter(
 const NavHeader = () => {
   return (
     <AppBar className="navbar" title="Prevention Point" position="static">
-      <Toolbar>
+      <Toolbar className="navbar__toolbar">
         <img
           src={PreventionPointLogo}
           alt="Prevention Point Logo"
