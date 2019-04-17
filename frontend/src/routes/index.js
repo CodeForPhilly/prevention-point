00002446ce8b9dadceb7ea79f072aed004c74f53
grahamalama <graham.beckley@gmail.com>
2019-04-17 19:44:57 -0400

    Table component and new layout

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import LoginForm from "../components/LoginForm"
-import ParticipantSearch from "../components/ParticipantSearch"
 import { Route } from "react-router-dom"
 import PrivateRoute from "../routes/PrivateRoute"
+import ParticipantQueue from "../containers/ParticipantQueue"
 
 const Routes = () => {
   return (
     <div>
       <Navbar />
-      <PrivateRoute exact path="/" component={ParticipantSearch} />
+      <PrivateRoute exact path="/" component={ParticipantQueue} />
       <Route path="/login" component={LoginForm} />
     </div>
   )
