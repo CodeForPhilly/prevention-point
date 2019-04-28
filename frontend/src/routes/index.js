8e270fd90e64252b464f295c34a3dcbda76affa1
grahamalama <graham.beckley@gmail.com>
2019-04-28 11:12:39 -0400

    Rename and reorganize queues container component

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,13 +3,13 @@ import Navbar from "../components/Navbar"
 import LoginForm from "../components/LoginForm"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import PrivateRoute from "../routes/PrivateRoute"
-import ParticipantQueue from "../containers/ParticipantQueue"
+import ServicesQueues from "./ServicesQueues"
 
 const Routes = () => {
   return (
     <Router>
       <Navbar />
-      <PrivateRoute exact path="/" component={ParticipantQueue} />
+      <PrivateRoute exact path="/" component={ServicesQueues} />
       <Route path="/login" component={LoginForm} />
     </Router>
   )
