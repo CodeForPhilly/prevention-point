6dc2d0534eff31ba38fd893435202bd70f64ea1d
grahamalama <graham.beckley@gmail.com>
2019-05-01 19:38:43 -0400

    Configure FE server for local development Calls to the frontend development server's /api path will proxy to the backend server

---
+++ frontend/devServer.js
@@ -0,0 +1,20 @@
+const ParcelProxyServer = require("parcel-proxy-server")
+
+// configure the proxy server
+// https://github.com/charlieduong94/parcel-proxy-server
+const server = new ParcelProxyServer({
+  entryPoint: "./public/index.html",
+  proxies: {
+    "/api": {
+      target: "http://127.0.0.1:8000/",
+    },
+  },
+})
+
+server.bundler.on("buildEnd", () => {
+  console.log("Build completed!") //eslint-disable-line
+})
+
+server.listen(1234, () => {
+  console.log("Parcel proxy server has started") //eslint-disable-line
+})
