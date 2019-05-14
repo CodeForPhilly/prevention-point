4ff26e2f6bb356ddef8e38a183e2658c9cd50e6b
Thomas Vendetta <thomas@vendetta.io>
2019-05-13 22:22:11 -0400

    Add untracked viewsets.py

---
+++ core/viewsets.py
@@ -0,0 +1,6 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from core.permissions import HasGroupPermission
+
+class ModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [HasGroupPermission, IsAuthenticated]
