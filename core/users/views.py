dc5a90ef2af1c916346546a483b34da19c0d2422
grahamalama <graham.beckley@gmail.com>
2019-05-21 19:02:51 -0400

    Remove unused imports

--- core/users/views.py
+++ core/users/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from core.users.serializers import UserSerializer, GroupSerializer
-from rest_framework.response import Response
 from core.permissions import HasGroupPermission
 
 
