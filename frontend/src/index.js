9abc03d93e3b277415271f19539dee5ac2d54f00
rob bee <robbeeio63@gmail.com>
2019-06-10 12:37:10 -0400

    font update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,77 @@ const theme = createMuiTheme({
   },
   typography: {
     useNextVariants: true,
+    fontFamily: ["Arial", "sans-serif"].join(","),
+
+    // https://material-ui.com/api/typography/
+    // Main/H1/White
+
+    h1: {
+      fontStyle: ["normal"],
+      fontSize: ["32px"],
+      lineheight: ["38px"],
+      fontWeight: ["bold"],
+      color: ["#FFFFFF"],
+    },
+
+    // Main/H1/Green
+    h2: {
+      fontSize: ["32px"],
+      fontWeight: ["bold"],
+      color: ["#336D7C"],
+    },
+
+    // header/link
+    h3: {
+      fontSize: ["13px"],
+      fontWeight: ["bold"],
+      color: ["#FFFFFF"],
+    },
+
+    // header/notification
+    h4: {
+      fontSize: ["12px"],
+      color: ["#000000"],
+    },
+
+    // sidebar
+    h5: {
+      fontSize: ["28px"],
+      fontWeight: ["bold"],
+      color: ["#336D7C"],
+    },
+
+    // sidebar 2
+    h6: {
+      fontSize: ["16px"],
+      fontWeight: ["bold"],
+      color: ["#292929"],
+    },
+
+    // form/instuctions
+    subtitle1: {
+      fontSize: ["20px"],
+      color: ["#2699FB"],
+    },
+
+    // form/H3
+    subtitle2: {
+      fontSize: ["16px"],
+      color: ["#336E7C"],
+    },
+
+    // form/H1
+    subtitle3: {
+      fontSize: ["16px"],
+      color: ["#336E7C"],
+    },
+
+    // form/Response/Grey
+    body1: {
+      fontSize: ["14px"],
+      //lineheight: [],
+      color: ["#BCBCBC"],
+    },
   },
 })
 
