e5b99291fc37f305496114ffeb9ad8e7bf19b0fe
Matt Usifer <matt@blackfynn.com>
2019-06-11 19:18:49 -0400

    Test with sqlite

--- preventionpoint/settings/common.py
+++ preventionpoint/settings/common.py
@@ -77,20 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'preventionpoint.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
