edbc2dc2b6c6205b24565f6bf975f59811f9dad6
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-06-21 16:11:00 -0400

    using dictionary over tuple to avoid list of 1 on service event

--- core/service_events/views.py
+++ core/service_events/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from core.models import ServiceEvent
 from core.service_events.serializer import ServiceEventAndAvailabilitySerializer, ServiceEventSerializer
+from core.services.serializers import ServiceSerializer
 from core.permissions import FRONT_DESK, CASE_MANAGER, ADMIN
 from core.models import Service 
 from collections import namedtuple
@@ -10,7 +11,7 @@ from core.permissions import HasGroupPermission
 from django.http import Http404
 from rest_framework import status
 
-EventsAndServices = namedtuple('EventsAndServices',('service_events', 'availability'))
+EventsAndServices = namedtuple('EventsAndServices',('service_events', 'availability'),rename=True)
 
 class ServiceEventViewSet(viewsets.ViewSet):
     """
@@ -33,29 +34,31 @@ class ServiceEventViewSet(viewsets.ViewSet):
         return Response(serializer.data)
   
     def retrieve(self, request, pk=None):
-            queryset = ServiceEvent.objects.filter(pk=pk)
+            queryset = ServiceEvent.objects.get(pk=pk)
+            event_serializer = ServiceEventSerializer(queryset)
+            print(event_serializer.data)
             if not queryset:
-                 return Response(status=status.HTTP_404_NOT_FOUND)
-            events_and_services = EventsAndServices(
-                service_events = queryset,
-                availability = Service.objects.all()
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            availability_queryset = Service.objects.all()
+            availability_serializer = ServiceSerializer(availability_queryset, many=True)
+            events_and_services = dict(
+                service_events = event_serializer.data,
+                availability = availability_serializer.data
             )
-            serializer = ServiceEventAndAvailabilitySerializer(events_and_services, context={"request": request})
-            return Response(serializer.data)
+            return Response(events_and_services)
          
     def create(self, request):
-        service_event=request.data
-        serializer = ServiceEventSerializer(data=service_event)
-        if serializer.is_valid():
-            serializer.save()
-            queryset = ServiceEvent.objects.filter(pk=serializer.data['id'])
-            events_and_services = EventsAndServices(
-                service_events = queryset,
-                availability = Service.objects.all()
+        event_serializer = ServiceEventSerializer(data=request.data)
+        if event_serializer.is_valid():
+            event_serializer.save()
+            availability_queryset = Service.objects.all()
+            availability_serializer = ServiceSerializer(availability_queryset, many=True)
+            events_and_services = dict(
+                service_events= event_serializer.data,
+                availability= availability_serializer.data
             )
-            event_availabilty_serializer = ServiceEventAndAvailabilitySerializer(events_and_services, context={"request": request})
-            return Response(event_availabilty_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(events_and_services, status=status.HTTP_201_CREATED)
+        return Response(event_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def update(self, request, pk=None):
         queryset = ServiceEvent.objects.filter(pk=pk)
