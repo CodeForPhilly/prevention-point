a14b14ab7269d48a883b4c7acae9dd46a079a8be
natalya diaz <nkgdiaz@gmail.com>
2019-07-08 11:36:37 -0400

    renamed to program availability

--- core/models/program_availability.py
+++ core/models/program_availability.py
@@ -1,8 +1,8 @@
 from django.db import models
 from core.models import Program
 
-class ServiceAvailibility(models.Model):
-    service_id = models.ForeignKey(Service, on_delete=models.CASCADE)
+class ProgramAvailibility(models.Model):
+    program_id = models.ForeignKey(Program, on_delete=models.CASCADE)
     day_of_week = models.CharField(max_length = 10)
     start_time = models.DateField()
     end_time = models.DateField()
