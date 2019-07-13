cc754268677caa5e0935d9c3b0d54072759ec7cf
grahamalama <graham.beckley@gmail.com>
2019-07-13 13:42:40 -0400

    Add QueueStore

--- frontend/src/stores/RootStore.js
+++ frontend/src/stores/RootStore.js
@@ -1,8 +1,10 @@
 import { createContext } from "react"
 import { AuthStore } from "./AuthStore"
+import { QueueStore } from "./QueueStore"
 
 export class RootStore {
   authStore = new AuthStore(this)
+  QueueStore = new QueueStore(this)
 }
 
 export const rootStoreContext = createContext(new RootStore())
