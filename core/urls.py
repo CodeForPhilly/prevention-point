bf8ecdac553a251d6d04c721410111f943143ff5
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-07-21 15:32:10 -0400

    created and loaded fixtures, endpoint returns all program availability, need to filter for program up the namespace

--- core/urls.py
+++ core/urls.py
@@ -52,8 +52,12 @@ urlpatterns = [
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('api/programs/<int:program_id>/program-availability/', program_availability_views.ProgramAvailabilityViewSet.as_view({'get':'list'})),
+<<<<<<< HEAD
     path('api/programs/<int:program_id>/program-availability/<int:pk>/', program_availability_views.ProgramAvailabilityViewSet.as_view({'put':'update'})),
 
+=======
+    path('api/programs/<int:program_id>/program-availability/', program_availability_views.ProgramAvailabilityViewSet.as_view({'patch':'update'})),
+>>>>>>> created and loaded fixtures, endpoint returns all program availability, need to filter for program up the namespace
     path('api/programs/<int:program_id>/queue/', queues_views.QueueViewSet.as_view({'get':'retrieve'})),
     # above only 'registers' the retrieve path. to use other CRUD actions, must pass that explicitly to .as_view in another register
     path('swagger/', schema_view),
