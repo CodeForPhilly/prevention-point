0bacca049b53467f9331febd4a5233e7fb9e7e89
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-07-23 21:48:02 -0400

    availability listed by program id

--- core/program_availability/views.py
+++ core/program_availability/views.py
@@ -1,4 +1,6 @@
 from core.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.response import Response
 from core.models import ProgramAvailability
 from core.program_availability.serializer import ProgramAvailabilitySerializer
 from core.permissions import FRONT_DESK, CASE_MANAGER, ADMIN
@@ -11,4 +13,15 @@ class ProgramAvailabilityViewSet(ModelViewSet):
         'update': [FRONT_DESK, CASE_MANAGER, ADMIN],
         'list': [FRONT_DESK, CASE_MANAGER, ADMIN]
     }
-  
+
+
+
+    def list(self, request, program_id=None):
+      availability_queryset = ProgramAvailability.objects.filter(program_id=program_id)
+      if availability_queryset.exists():
+        availibility_by_program = ProgramAvailabilitySerializer(availability_queryset, many=True).data
+      else: 
+        return Response(status=status.HTTP_404_NOT_FOUND)
+      return Response(availibility_by_program)
+
+# import pdb; pdb.set_trace()
