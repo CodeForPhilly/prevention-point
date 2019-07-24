de3c96efbd6f3cf09325d043089a7494bf61c68e
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-07-24 08:54:13 -0400

    update route functioning, with readonly fields on progrma and day_of_week, (program_availability)

--- core/program_availability/serializer.py
+++ core/program_availability/serializer.py
@@ -7,4 +7,4 @@ class ProgramAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProgramAvailability
         fields = ('id', 'program', 'day_of_week', 'start_time', 'end_time' ) 
-
+        read_only_fields = ['day_of_week', 'program']
