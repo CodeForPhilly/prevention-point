1aea8f332ce278bdecaac66e90cb2a3ca3a7f0b9
Harry Hummel <hhhummel@gmail.com>
2019-08-07 15:18:44 -0400

    Remove add_users_to_groups, suppress output, make valid fake email addresses, add full_clean where missing

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -6,6 +6,7 @@ from faker import Faker
 from faker.providers import BaseProvider
 from core.models import UrineDrugScreen, Medication, Participant, Gender, Race, Program, Service, Visit
 from core.permissions import CASE_MANAGER, FRONT_DESK, ADMIN
+
 import random
 import pytz
 
@@ -18,7 +19,7 @@ DEFAULT_USER_IDS = {
         'case_manager': ['5', '6', '7', '8'],
         'admin': ['9', '10']
     }
-
+DEFAULT_DOMAIN = "prevention-point"
 DEFAULT_NUMBER_PARTICIPANTS = 10
 DEFAULT_NUMBER_VISITS = 20
 
@@ -49,9 +50,7 @@ class MedsProvider(BaseProvider):
         meds = [u'film', u'tab', u'vivitrol']
         return random.choices(meds)
 
-
 fake.add_provider(MedsProvider)
-
 
 class FrequencyProvider(BaseProvider):
     __provider__ = "frequency"
@@ -61,24 +60,23 @@ class FrequencyProvider(BaseProvider):
         frequency = [7, 14, 28, 21, 3]
         return random.choices(frequency)
 
-
 fake.add_provider(FrequencyProvider)
 
 def run_seed(self):
     call_command('migrate')
     call_command('flush')
-    create_groups()
-    create_users()
-    #add_users_to_groups()
+    create_groups(output=False)
+    create_users(output=False)
     create_participants(uds=False, medication=False)
-    create_programs()
-    create_visits()
+    create_programs(output=False)
+    create_visits(output=False)
 
 def create_users(output=True):
+    '''Create fake users for each group'''
     for group in DEFAULT_GROUPS:
         for id in DEFAULT_USER_IDS[group]:
-            user_name = "{}_{}".format(group, id)
-            email = "{}@{}.com".format(user_name, group)
+            user_name = "{}-{}".format(group, id)
+            email = "{}@{}.com".format(user_name, DEFAULT_DOMAIN)
             user = User.objects.create_user(username=user_name, email=email)
             user.set_password(DEFAULT_DEV_ENV_PASS)
 
@@ -86,6 +84,7 @@ def create_users(output=True):
                 user.is_superuser=True
                 user.is_staff=True
 
+            user.full_clean()
             user.save()
 
             #Assign user to group
@@ -96,22 +95,14 @@ def create_users(output=True):
                 print("Created user: {}".format(email))
 
 def create_groups(output=True):
+    '''Create groups from DEFAULT_GROUPS'''
     for group in DEFAULT_GROUPS:
-        print("Group = {}".format(group))
+        if output == True:
+            print("Group = {}".format(group))
         Group.objects.get_or_create(name=group)
 
-def add_users_to_groups(output=True):
-    """
-    adds user to group of same name
-    """
-    print("Okay to here!")
-    for group in DEFAULT_GROUPS:
-        user = User.objects.get(username=group)
-        role_title = Group.objects.get(name=group)
-        user.groups.add(role_title)
-    print("Still okay!")
-
 def create_participants(uds=True, medication=True):
+    '''Create a fake participant, and optionally associated UDS and meds'''
     gender_list = list(Gender)
     race_list = list(Race)
 
@@ -142,6 +133,7 @@ def random_bool():
     return bool(random.getrandbits(1))
 
 def create_medication(participant):
+    '''Create a meds for a given participant'''
     meds = Medication(
         participant=participant,
         medication_name=fake.meds()[0],
@@ -152,6 +144,7 @@ def create_medication(participant):
     meds.save()
 
 def create_uds_results(participant):
+    '''Create a fake UDS for a given participant'''
     for _ in range(random.randint(2,10)):
         test_date = fake.date_time_between(start_date=participant.start_date, end_date='+5y')
 
@@ -179,9 +172,11 @@ def create_uds_results(participant):
         uds.save()
 
 def create_programs(output=True):
+    '''Create programs and services from the DEFAULT_PROGRAMS dictionary, reflecting actual P-P organization'''
     for program, services in DEFAULT_PROGRAMS.items():
       p = Program()
       p.name = program
+      p.full_clean()
       p.save()
 
       if output:
@@ -192,13 +187,14 @@ def create_programs(output=True):
           s.name = service
           s.program = p
           s.available = random_bool()
+          s.full_clean()
           s.save()
 
           if output:
               print("Created {}: '{}'". format(p.name, s.name))
 
 def create_visits(output=True):
-
+    '''Create fake visits, depending on Participants.  Uses UTC in created_at date_times'''
     participants = Participant.objects.all()
     programs = Program.objects.all()
 
@@ -208,8 +204,9 @@ def create_visits(output=True):
         v.created_at = pytz.utc.localize(fake.date_time())
         v.program = random.choice(programs)
         v.notes = fake.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None)
+        v.full_clean()
         v.save()
 
-        if output == True:
+        if output:
             print("Created visit: {} {}, {}, {}, {}". format(v.participant.first_name, v.participant.last_name, v.created_at, v.program.name, v.notes))
 
