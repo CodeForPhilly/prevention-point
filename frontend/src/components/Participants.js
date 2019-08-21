84fa304dcc417909df84604f9bdfc7a88c0be673
Harry Hummel <hhhummel@gmail.com>
2019-08-20 22:53:23 -0400

    No errors, but no data either

--- frontend/src/components/Participants.js
+++ frontend/src/components/Participants.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react"
-import { participantStore } from "../stores/ParticipantStore"
+import participantStore from "../stores/ParticipantStore"
 
 export default
 @observer
@@ -8,10 +8,13 @@ class Participant extends React.Component {
   render() {
     const store = participantStore
     return (
-      <div className="participants">
-        {store.data.map(participant => (
-          <p key={participant}>{participant.first_name}</p>
-        ))}
+      <div>
+        <p>Participants</p>
+        <div className="participants">
+          {store.participants.map(participant => (
+            <p key={participant}>{participant.first_name}yes</p>
+          ))}
+        </div>
       </div>
     )
   }
