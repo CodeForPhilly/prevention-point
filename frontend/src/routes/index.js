ec3fa2df3e81fc6e7090cc11123145d05534799a
Ryan Lacon <ryan.lacon@gmail.com>
2019-08-22 09:31:25 -0400

    Added fields to ParticipantInfo component for name, date of birth, and race

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import LoginForm from "../components/LoginForm"
+import ParticipantInfo from "../components/ParticipantInfo"
 import ParticipantSearch from "../components/ParticipantSearch"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import PrivateRoute from "../routes/PrivateRoute"
@@ -11,6 +12,7 @@ const Routes = () => {
       <Navbar />
       <PrivateRoute exact path="/" component={ParticipantSearch} />
       <Route path="/login" component={LoginForm} />
+      <Route path="/main" component={ParticipantInfo} />
     </Router>
   )
 }
