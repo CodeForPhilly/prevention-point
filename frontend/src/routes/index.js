95245cf5d88b0bc76ff58697720295087e204438
Harry Hummel <hhhummel@gmail.com>
2019-08-28 10:52:28 -0400

    Put back routes/index.js from master

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import LoginForm from "../components/LoginForm"
+import Participant from "../components/Participants"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import PrivateRoute from "../routes/PrivateRoute"
+import ServicesQueues from "./ServicesQueues"
+
+const Routes = () => {
+  return (
+    <Router>
+      <Navbar />
+      <PrivateRoute exact path="/participants" component={Participant} />
+      <PrivateRoute exact path="/" component={ServicesQueues} />
+      <Route path="/login" component={LoginForm} />
+    </Router>
+  )
+}
+
+export default Routes
