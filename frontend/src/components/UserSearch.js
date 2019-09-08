bb4c94dfd6609a4ba194a418970196843f37152b
jkeary <jkeary@gmail.com>
2019-09-08 14:42:33 -0400

    adding useState for hook based state updating in user search functional component

--- frontend/src/components/UserSearch.js
+++ frontend/src/components/UserSearch.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "../scss/participant-search.scss"
 import FormGroup from "@material-ui/core/FormGroup"
 import FormControl from "@material-ui/core/FormControl"
@@ -8,6 +8,12 @@ import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 
 const UserSearch = () => {
+  const inputState = useState({
+    userId: "",
+    firstName: "",
+    lastName: "",
+  })
+
   return (
     <div className="participant-search">
       <div align="center">
@@ -31,7 +37,20 @@ const UserSearch = () => {
           <FormGroup className="participant-search__input">
             <FormControl>
               <InputLabel htmlFor="user_id">User ID</InputLabel>
-              <Input id="user_id" name="user_id" value="" required />
+              <Input
+                id="user_id"
+                name="user_id"
+                required
+                value={inputState[0].userId}
+                onChange={event => {
+                  const newUserId = event.target.value
+                  inputState[1](prevInputState => ({
+                    userId: newUserId,
+                    firstName: prevInputState.firstName,
+                    lastName: prevInputState.lastName,
+                  }))
+                }}
+              />
             </FormControl>
           </FormGroup>
           <Typography
@@ -45,13 +64,39 @@ const UserSearch = () => {
           <FormGroup className="participant-search__input">
             <FormControl>
               <InputLabel htmlFor="first_name">First Name</InputLabel>
-              <Input id="first_name" name="first_name" value="" required />
+              <Input
+                id="first_name"
+                name="first_name"
+                required
+                value={inputState[0].firstName}
+                onChange={event => {
+                  const newFirstName = event.target.value
+                  inputState[1](prevInputState => ({
+                    userId: prevInputState.userId,
+                    firstName: newFirstName,
+                    lastName: prevInputState.lastName,
+                  }))
+                }}
+              />
             </FormControl>
           </FormGroup>
           <FormGroup className="participant-search__input">
             <FormControl>
               <InputLabel htmlFor="last_name">Last Name</InputLabel>
-              <Input id="last_name" name="last_name" value="" required />
+              <Input
+                id="last_name"
+                name="last_name"
+                required
+                value={inputState[0].lastName}
+                onChange={event => {
+                  const newLastName = event.target.value
+                  inputState[1](prevInputState => ({
+                    userId: prevInputState.userId,
+                    firstName: prevInputState.firstName,
+                    lastName: newLastName,
+                  }))
+                }}
+              />
             </FormControl>
           </FormGroup>
           <Button type="submit" variant="contained" style={{ marginTop: 30 }}>
