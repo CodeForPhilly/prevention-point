387dbff8ca2402d39439068d5f453bbe3b153fb0
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-09-10 21:53:01 -0700

    addded mapping table and updated seed on visit

--- core/models/visit.py
+++ core/models/visit.py
@@ -1,12 +1,12 @@
 from django.db import models
 from core.models.participant import Participant
-from core.models.program import Program
+from core.models.program_service_map import ProgramServiceMap
 from django.utils import timezone
 
 class Visit(models.Model):
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
-    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    program_service_map = models.ForeignKey( ProgramServiceMap, null=True, on_delete=models.CASCADE)
     notes = models.TextField("Visit Notes", null= True, blank=True)
 
 
