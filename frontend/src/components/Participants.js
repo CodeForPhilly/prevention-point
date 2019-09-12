4828fd99935404ad8a8318ba9a66334640c4816b
Angel <angel.m.young24@gmail.com>
2019-09-12 19:48:05 -0400

    Added UI in Participants.js

--- frontend/src/components/Participants.js
+++ frontend/src/components/Participants.js
@@ -1,32 +1,69 @@
 import React from "react"
 import { observer } from "mobx-react"
-import participantStore from "../stores/ParticipantStore"
+import ParticipantStore from "../stores/ParticipantStore"
+
+import Breadcrumbs from "@material-ui/core/Breadcrumbs"
+import Typography from "@material-ui/core/Typography"
+import Link from "@material-ui/core/Link"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
 
 export default
 @observer
 class Participant extends React.Component {
-  constructor(props) {
-    super(props)
-    this.store = participantStore
-    this.store.getParticipants()
+  componentDidMount() {
+    ParticipantStore.getParticipants()
   }
 
   render() {
-    const store = participantStore
     return (
       <div>
-        <p>Participants</p>
+        <Breadcrumbs separator="›" aria-label="breadcrumb">
+          <Link color="inherit" href="/">
+            Home
+          </Link>
+          <Typography color="textPrimary">Search Results</Typography>
+        </Breadcrumbs>
+        <Typography variant="h5" color="textPrimary">
+          Participants
+        </Typography>
         <div className="participants">
-          {store.participants.map((participant, index) => (
-            <p key={index}>
-              {participant.first_name} {participant.last_name}
-              {participant.pp_id}
-            </p>
-          ))}
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography>PPID</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>First Name</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>Last Name</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ParticipantStore.participants.map((participant, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Typography>{participant.pp_id} </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>{participant.first_name}</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>{participant.last_name}</Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
         <div>
-          <p>Participant Search by ID</p>
-          <p>{this.store.filter("T9FN3", null, null).first_name}</p>
+          <p>{ParticipantStore.filter("T9FN3", null, null).first_name}</p>
         </div>
       </div>
     )
