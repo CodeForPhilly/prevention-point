d564eaa5902b4304da583dc088d4df3a5cec9309
Angel <angel.m.young24@gmail.com>
2019-09-16 10:52:04 -0400

    updated UI

--- frontend/src/components/Participants.js
+++ frontend/src/components/Participants.js
@@ -10,6 +10,8 @@ import TableBody from "@material-ui/core/TableBody"
 import TableCell from "@material-ui/core/TableCell"
 import TableHead from "@material-ui/core/TableHead"
 import TableRow from "@material-ui/core/TableRow"
+import Fab from "@material-ui/core/Fab"
+import AddIcon from "@material-ui/icons/Add"
 
 export default
 @observer
@@ -35,6 +37,9 @@ class Participant extends React.Component {
             <TableHead>
               <TableRow>
                 <TableCell>
+                  <Typography>#</Typography>
+                </TableCell>
+                <TableCell>
                   <Typography>PPID</Typography>
                 </TableCell>
                 <TableCell>
@@ -43,11 +48,23 @@ class Participant extends React.Component {
                 <TableCell>
                   <Typography>Last Name</Typography>
                 </TableCell>
+                <TableCell>
+                  <Typography>Address</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>DOB</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>Add</Typography>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {ParticipantStore.participants.map((participant, index) => (
                 <TableRow key={index}>
+                  <TableCell>
+                    <Typography>Number</Typography>
+                  </TableCell>
                   <TableCell>
                     <Typography>{participant.pp_id} </Typography>
                   </TableCell>
@@ -57,11 +74,25 @@ class Participant extends React.Component {
                   <TableCell>
                     <Typography>{participant.last_name}</Typography>
                   </TableCell>
+                  <TableCell>
+                    <Typography>Address</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>DOB</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Fab color="primary" size="small" aria-label="add">
+                      <AddIcon />
+                    </Fab>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </div>
+        <Fab color="primary" aria-label="add" size="large">
+          <AddIcon />
+        </Fab>
         <div>
           <p>{ParticipantStore.filter("T9FN3", null, null).first_name}</p>
         </div>
