928cc890479031de69cd3691d220c185e7d9a05b
jkeary <jkeary@gmail.com>
2019-09-19 18:12:46 -0400

    basic ui and property testing of the Login form

--- frontend/test/Login.test.js
+++ frontend/test/Login.test.js
@@ -1,23 +1,76 @@
 import React from "react"
 import { shallow } from "enzyme"
 import LoginForm from "../src/components/LoginForm"
+import Button from "@material-ui/core/Button"
 
-describe("<LoginForm /> with no props", () => {
-  const container = shallow(<LoginForm />)
+describe("<LoginForm />", () => {
+  // create initial props that get passed in
+  const initialProps = {
+    location: {
+      state: {
+        from: {
+          pathname: "/",
+        },
+      },
+    },
+  }
+
+  // Render the login form component, pass in props. (Shallow method renders the component without its children.)
+  const wrapper = shallow(<LoginForm {...initialProps} />)
+
+  // UI Integrity test
   it("should match the snapshot", () => {
-    expect(container.html()).toMatchSnapshot()
+    // snapshots are text references of the html of the rendered component.
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // Grab the UI pieces you want to test
+  const usernameInput = wrapper.find("#username")
+  const passwordInput = wrapper.find("#password")
+  const signInButton = wrapper.find(Button)
+
+  it("should have a username inputs", () => {
+    expect(usernameInput.length).toEqual(1)
+  })
+
+  it("should have the expected props on the username field", () => {
+    expect(usernameInput.props()).toEqual({
+      id: "username",
+      name: "username",
+      value: "",
+      onChange: expect.any(Function),
+      required: true,
+    })
   })
 
   it("should have a password field", () => {
-    /* Similar to above */
+    expect(passwordInput.length).toEqual(1)
   })
-  it("should have proper props for password field", () => {
-    /* Trimmed for less lines to read */
+
+  it("should have the expected props on the password field", () => {
+    expect(passwordInput.props()).toEqual({
+      id: "password",
+      name: "password",
+      value: "",
+      type: "password",
+      onChange: expect.any(Function),
+      required: true,
+    })
   })
+
   it("should have a submit button", () => {
-    /* */
+    expect(signInButton.length).toEqual(1)
   })
-  it("should have proper props for submit button", () => {
-    /* */
+
+  it("should have the expected props on the button", () => {
+    expect(signInButton.props()).toEqual({
+      type: "button",
+      variant: "contained",
+      style: expect.objectContaining({
+        marginTop: "10px",
+      }),
+      onClick: expect.any(Function),
+      children: "Sign In",
+    })
   })
 })
