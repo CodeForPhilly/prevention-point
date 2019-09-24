b4a53c541dc2d6b70ce4f5cd28caec643c7ea25f
rob bee <robbeeio63@gmail.com>
2019-09-24 12:04:37 -0400

    update ServiceQueques

--- frontend/src/routes/ServicesQueues.js
+++ frontend/src/routes/ServicesQueues.js
@@ -1,87 +1,141 @@
-import React, { useState } from "react"
-import PropTypes from "prop-types"
+import React from "react"
+import clsx from "clsx"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Divider from "@material-ui/core/Divider"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
+import ChevronRightIcon from "@material-ui/icons/ChevronRight"
+import Navbar from "../components/Navbar"
+import UserSearch from "../components/UserSearch"
 import AllQueues from "../components/AllQueues"
-import ParticipantSearch from "../components/ParticipantSearch"
-import {
-  Grid,
-  Paper,
-  IconButton,
-  Typography,
-  Drawer,
-  Hidden,
-} from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
-import SearchIcon from "@material-ui/icons/Search"
 
-const drawerWidth = 240
+const drawerWidth = 300
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
   },
-  searchButton: {
-    marginRight: 20,
-    [theme.breakpoints.up("lg")]: {
-      display: "none",
-    },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: "#F2FCFF",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }))
 
-const ParticipantQueue = () => {
+export default function PersistentDrawerLeft() {
   const classes = useStyles()
-  const [drawerOpen, setDrawerOpen] = useState(false)
-  const toggleDrawer = open => () => {
-    setDrawerOpen(open)
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+
+  function handleDrawerOpen() {
+    setOpen(true)
   }
+
+  function handleDrawerClose() {
+    setOpen(false)
+  }
+
   return (
-    <div>
-      <Hidden smDown implementation="css">
-        <Drawer
-          variant="temporary"
-          anchor={"left"}
-          open={drawerOpen}
-          onClose={toggleDrawer(false)}
-          className={classes.drawer}
-          ModalProps={{
-            keepMounted: true,
-          }}
-        >
-          <ParticipantSearch />
-        </Drawer>
-      </Hidden>
-      <Grid
-        container
-        style={{ justifyContent: "space-around" }}
-        className="participant-dashboard"
-        spacing={9}
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
       >
-        <Hidden mdDown>
-          <Grid item md={3}>
-            <ParticipantSearch />
-          </Grid>
-        </Hidden>
-        <Grid item md>
-          <Paper>
-            <IconButton
-              onClick={toggleDrawer(true)}
-              className={classes.searchButton}
-            >
-              <SearchIcon />
-              <Typography>Participant Search</Typography>
-            </IconButton>
-            <AllQueues />
-          </Paper>
-        </Grid>
-      </Grid>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Navbar />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <UserSearch />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <AllQueues />
+      </main>
     </div>
   )
 }
-
-ParticipantQueue.propTypes = {
-  classes: PropTypes.object,
-}
-
-export default ParticipantQueue
