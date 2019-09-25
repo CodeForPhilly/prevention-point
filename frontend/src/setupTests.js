7bb20d1a8ed99195de8d58b65f1ac2d8259401e5
jkeary <jkeary@gmail.com>
2019-09-25 11:40:47 -0400

    fixing the Warning: useLayoutEffect does nothing on the server warning on yarn test

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,3 +2,6 @@ import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 
 configure({ adapter: new Adapter() })
+
+import React from "react"
+React.useLayoutEffect = React.useEffect
