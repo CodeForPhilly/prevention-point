351b5b067aeb2ffc2e87a6889d0d96e08c37361d
jkeary <jkeary@gmail.com>
2019-09-25 11:43:13 -0400

    explanation comment

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,5 +3,6 @@ import Adapter from "enzyme-adapter-react-16"
 
 configure({ adapter: new Adapter() })
 
+// Fix to a material ui related issue that jest doesnt like.  These two lines suppress a warning about using useLayoutEffect
 import React from "react"
 React.useLayoutEffect = React.useEffect
