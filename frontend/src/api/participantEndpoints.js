20296c4e9d0e4d7e9d4969f891c7c66e71c6b20f
James Keary <jkeary@gmail.com>
2019-10-07 18:18:20 -0400

    Deleted participantEndpoints.js from this branch
    deleted a front end non pr-ed file from this  branch that it shouldnt have been on

--- frontend/src/api/participantEndpoints.js
+++
@@ -1,7 +0,0 @@
-export const getParticipants = api => async () => await api.get("participants/")
-
-export const getParticipantById = api => async id =>
-  await api.get(`participants/${id}/`)
-
-export const getParticipantByName = api => async (firstname, lastname) =>
-  await api.get(`participants/?first_name=${firstname}&last_name=${lastname}`)
