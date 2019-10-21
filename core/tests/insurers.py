146b1c409033911e10d025a9fffbef34ee18b396
Harry Hummel <hhhummel@gmail.com>
2019-10-21 16:05:06 -0400

    Correct typos

--- core/tests/insurers.py
+++ core/tests/insurers.py
@@ -3,7 +3,7 @@ from core.tests.base import BaseTestCase
 from core.models.insurer import Insurer
 
 
-class InsurerssTestCase(BaseTestCase):
+class InsurersTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.seed_fake_users()
@@ -25,7 +25,7 @@ class InsurerssTestCase(BaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Insurer.objects.count(), 1)
 
-    def test_insurerss_api_when_authed_as_front_desk(self):
+    def test_insurers_api_when_authed_as_front_desk(self):
         headers = self.auth_headers_for_user('front_desk')
         response = self.client.get('/api/insurerss', follow=True, **headers)
 
