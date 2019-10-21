c67ffc2e7c6bfc40b713097194531b913adcea7e
jkeary <jkeary@gmail.com>
2019-10-21 02:16:35 -0400

    added loading state

--- frontend/src/components/ParticipantsList.js
+++ frontend/src/components/ParticipantsList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { Fragment, useContext, useEffect, useState } from "react"
 import { rootStoreContext } from "../stores/RootStore"
 import Breadcrumbs from "@material-ui/core/Breadcrumbs"
 import Typography from "@material-ui/core/Typography"
@@ -15,97 +15,106 @@ import { observer } from "mobx-react-lite"
 const ParticipantsList = observer(() => {
   const rootStore = useContext(rootStoreContext)
   const participantsStore = rootStore.ParticipantStore
+  const [isLoading, setIsLoading] = useState(false)
 
   // useEffect is a hook that gets called after every render/re-render.  Empty array second argument prevents it from running again.
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true)
       await participantsStore.getParticipants()
+      setIsLoading(false)
     }
     fetchData()
   }, [])
 
   return (
-    <div>
-      <Breadcrumbs separator="›" aria-label="breadcrumb">
-        <Link color="inherit" href="/">
-          Home
-        </Link>
-        <Typography color="textPrimary">Search Results</Typography>
-      </Breadcrumbs>
-      <Typography variant="h5" color="textPrimary">
-        Participants
-      </Typography>
-      <div className="participants">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography>#</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>PPID</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>First Name</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>Last Name</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>Gender</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>DOB</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>Race</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>Add</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {participantsStore.participants.map((participant, index) => (
-              <TableRow key={index}>
-                <TableCell>
-                  <Typography>Number</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{participant.pp_id} </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{participant.first_name}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{participant.last_name}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>Gender</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>DOB</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>Race</Typography>
-                </TableCell>
-                <TableCell>
-                  <Fab color="primary" size="small" aria-label="add">
-                    <AddIcon />
-                  </Fab>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-      <Fab color="primary" aria-label="add" size="large">
-        <AddIcon />
-      </Fab>
-      <div>
-        <p>{participantsStore.filter("T9FN3", null, null).first_name}</p>
-      </div>
-    </div>
+    <Fragment>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          <Breadcrumbs separator="›" aria-label="breadcrumb">
+            <Link color="inherit" href="/">
+              Home
+            </Link>
+            <Typography color="textPrimary">Search Results</Typography>
+          </Breadcrumbs>
+          <Typography variant="h5" color="textPrimary">
+            Participants
+          </Typography>
+          <div className="participants">
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <Typography>#</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>PPID</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>First Name</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>Last Name</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>Gender</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>DOB</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>Race</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>Add</Typography>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {participantsStore.participants.map((participant, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <Typography>Number</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{participant.pp_id} </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{participant.first_name}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{participant.last_name}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>Gender</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>DOB</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>Race</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Fab color="primary" size="small" aria-label="add">
+                        <AddIcon />
+                      </Fab>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+          <Fab color="primary" aria-label="add" size="large">
+            <AddIcon />
+          </Fab>
+          <div>
+            <p>{participantsStore.filter("T9FN3", null, null).first_name}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   )
 })
 
