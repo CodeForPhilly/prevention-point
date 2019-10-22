7e471a8d963b78568f45ad2603e6a8731cb342c4
rob bee <robbeeio63@gmail.com>
2019-10-22 14:45:44 -0400

    new test files ParticipantSearch UserSearch

---
+++ frontend/test/SearchParticipant.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+import React from "react"
+import { shallow, configure } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+/* core testing */
+import Drawer from "@material-ui/core/Drawer"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import IconButton from "@material-ui/core/IconButton"
+/* component testing */
+import UserSearch from "../components/UserSearch"
+import Navbar from "../components/Navbar"
+/* eslint-disable no-unused-vars */
+/* createMount */
+
+/* createShallow */
+
+/* createRender */
+
+/**
+ * thinking out loud testing:
+ * Material-UI core works Ex: Drawer
+ * UserSearch Component works
+ * Navbar Component works
+ */
