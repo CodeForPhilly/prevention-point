49b9ef3156231f1d8a9c0fe1a75e6b862eba2617
Harry Hummel <hhhummel@gmail.com>
2019-10-22 08:45:41 -0400

    Use MUI testing

--- frontend/test/AllQueues.test.js
+++ frontend/test/AllQueues.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable quotes */
 import React from "react"
-import { shallow, configure } from "enzyme"
+import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 //import { render, fireEvent, cleanup } from "@testing-library/react"
-import { ThemeProvider } from "@material-ui/core/styles"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import { createMount } from "@material-ui/core/test-utils"
+//import { createMuiTheme } from "@material-ui/core/styles"
 
 import Button from "@material-ui/core/Button"
 import AppBar from "@material-ui/core/AppBar"
@@ -11,44 +13,40 @@ import AllQueues from "../src/components/AllQueues"
 
 configure({ adapter: new Adapter() })
 
-const theme = {}
+const theme = createMuiTheme({})
 
 describe("<AllQueues />", () => {
   let wrapper
   let buttons
-  let testing
   let bar
+  let mount
 
-  // Create initial props that get passed into the componena, (none in this case)
+  // Create initial props that get passed into the components, (none in this case)
   const initialProps = {}
 
   // Unit testing
   describe("Unit tests", () => {
     // what to do before each test
     beforeEach(() => {
-      wrapper = shallow(
+      mount = createMount()
+      wrapper = mount(
         <ThemeProvider theme={theme}>
           <AllQueues {...initialProps} />
         </ThemeProvider>
       )
       buttons = wrapper.find(Button)
-      testing = wrapper.find("TESTING")
       bar = wrapper.find(AppBar)
     })
 
     // what to do after each test
     afterEach(() => {
+      //mount.cleanup()
       jest.clearAllMocks()
     })
 
     // UI Integrity test
     it("should have 9 buttons", () => {
       expect(buttons.length).toEqual(9)
-    })
-
-    // UI Integrity test
-    it("should have 1 TESTING button", () => {
-      expect(testing.length).toEqual(1)
     })
 
     // UI Integrity test
