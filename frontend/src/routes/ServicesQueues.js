8fb3b46a588622e9d045bd1e6c14d8aed71c00f9
Harry Hummel <hhhummel@gmail.com>
2019-10-30 21:32:11 -0400

    Correct styles in ServicesQueues

--- frontend/src/routes/ServicesQueues.js
+++ frontend/src/routes/ServicesQueues.js
@@ -46,7 +46,7 @@ const useStyles = makeStyles(theme => ({
   },
   drawerPaper: {
     width: drawerWidth,
-    background: theme.background.default,
+    background: theme.palette.background.default,
   },
   drawerHeader: {
     display: "flex",
