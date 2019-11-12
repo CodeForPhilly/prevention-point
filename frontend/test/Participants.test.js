3751af4673ee76f406e7784229c95a3643b711ca
jkeary <jkeary@gmail.com>
2019-11-12 12:08:08 -0500

    fixed the tests

--- frontend/test/Participants.test.js
+++ frontend/test/Participants.test.js
@@ -1,18 +1,18 @@
 import { configure, shallow } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 import React from "react"
-import Participants from "../src/components/Participants"
+import ParticipantsList from "../src/components/ParticipantsList"
 import Breadcrumbs from "@material-ui/core/Breadcrumbs"
 import TableCell from "@material-ui/core/TableCell"
 import Typography from "@material-ui/core/Typography"
 
 configure({ adapter: new Adapter() })
 
-describe("<Participants />", () => {
+describe("<ParticipantsList />", () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallow(<Participants />)
+    wrapper = shallow(<ParticipantsList />)
   })
 
   it("should render a Breadcrumbs Material-UI component", () => {
@@ -20,12 +20,12 @@ describe("<Participants />", () => {
   })
 
   // the reason for 7 and not all the other cells, is because the initial render only has 7 table cells that are the header cells.
-  it("should render 7 TableCell Material-UI components on initial render", () => {
+  it("should render 8 TableCell Material-UI components on initial render", () => {
     // wrapper.setProps({prop: value}) for setting any initial props
-    expect(wrapper.find(TableCell)).toHaveLength(7)
+    expect(wrapper.find(TableCell)).toHaveLength(8)
   })
 
-  it("should render 7 TableCell Material-UI components on initial render", () => {
+  it("should render 8 TableCell Material-UI components on initial render", () => {
     // wrapper.setProps({prop: value}) for setting any initial props
     expect(wrapper.contains(<Typography>#</Typography>))
   })
