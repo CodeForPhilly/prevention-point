51ebe4579e838d9b4d430c0b4e85345aa9d6cd04
Chester ONeill <c.oneill.codes@gmail.com>
2020-02-19 02:06:31 -0500

    Clean up endpoint code a bit

--- frontend/src/api/programEndpoints.js
+++ frontend/src/api/programEndpoints.js
@@ -1,4 +1,4 @@
-export const getPrograms = api => async () => await api.get("programs/")
-export const getProgram = api => async id => await api.get(`programs/${id}/`)
+export const getPrograms = api => async () => api.get("programs/")
+export const getProgram = api => async id => api.get(`programs/${id}/`)
 export const patchProgram = api => async (id, body) =>
-  await api.patch(`programs/${id}/`, body)
+  api.patch(`programs/${id}/`, body)
