22386c57f6d3b686514f332822508adf204ce339
Chester ONeill <coneillcodes@gmail.com>
2020-07-25 22:48:59 -0400

    Batch mobx renders https://github.com/mobxjs/mobx-react-lite#observer-batching

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import "mobx-react-lite/batchingForReactDom"
 
 //old setup
 //ReactDOM.render(<App />, document.getElementById("app"))
