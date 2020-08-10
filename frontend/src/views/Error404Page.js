c94dfe6d660bb653ce112b9b1d5a8e434960e535
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2020-08-10 01:25:53 -0400

    Use custom Typography

--- frontend/src/views/Error404Page.js
+++ frontend/src/views/Error404Page.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles"
 import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
+import PrevPointHeading from "../components/Typography/PrevPointHeading"
+import PrevPointTitle from "../components/Typography/PrevPointTitle"
 import PrevPointButton from "../components/PrevPointButton"
 
 const useStyles = makeStyles(theme => ({
@@ -12,9 +13,6 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center",
     marginTop: theme.spacing(4),
   },
-  h1: {
-    fontWeight: "500",
-  },
 }))
 
 const Error404Page = () => {
@@ -22,12 +20,8 @@ const Error404Page = () => {
 
   return (
     <Container className={classes.root}>
-      <Typography variant="h1" color="primary" className={classes.h1}>
-        404
-      </Typography>
-      <Typography variant="h2" color="primary">
-        Page Not Found
-      </Typography>
+      <PrevPointHeading>404</PrevPointHeading>
+      <PrevPointTitle>Page Not Found</PrevPointTitle>
       <PrevPointButton component={Link} to="/">
         Go to Queue Page
       </PrevPointButton>
