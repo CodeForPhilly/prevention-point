7c4a73cf65253f6869be39ddcc97a6590d1503d8
Chester ONeill <coneillcodes@gmail.com>
2020-08-21 19:00:39 -0400

    Just some code cleanup

--- frontend/src/components/ParticipantTableComponent/PrevPointTableHead.js
+++ frontend/src/components/ParticipantTableComponent/PrevPointTableHead.js
@@ -5,13 +5,13 @@ import TableCell from "@material-ui/core/TableCell"
 import TableHead from "@material-ui/core/TableHead"
 import PrevPointTitle from "../Typography/PrevPointTitle"
 
-const PrevPointTableHead = props => {
+const PrevPointTableHead = ({ headerTitles }) => {
   return (
     <TableHead aria-label="thead">
       <TableRow>
-        {props.headerTitles && props.headerTitles.length > 0
-          ? props.headerTitles.map((title, index) => (
-              <TableCell key={index} aria-label="cell">
+        {headerTitles && headerTitles.length > 0
+          ? headerTitles.map((title, index) => (
+              <TableCell key={`${title}_${index}`} aria-label="cell">
                 <PrevPointTitle>{title}</PrevPointTitle>
               </TableCell>
             ))
