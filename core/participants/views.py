46cdf37e2ae42afcb11e992fa129e44962c0474b
JackRyan1989 <jack.jackryan@gmail.com>
2020-10-10 16:31:25 -0400

    Updated participant views to return 404 when queryset does not exist.

--- core/participants/views.py
+++ core/participants/views.py
@@ -1,6 +1,8 @@
 from core.viewsets import ModelViewSet
 from core.models import Participant
 from core.participants.serializers import ParticipantSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 class ParticipantViewSet(ModelViewSet):
     """
@@ -19,11 +21,11 @@ class ParticipantViewSet(ModelViewSet):
         sep_id = self.request.query_params.get('sep_id', None)
         queryset = Participant.objects.all()
         if first_name is not None:
-            queryset = queryset.filter(first_name__icontains=first_name)
+            queryset = queryset.filter(first_name__istartswith=first_name)
         if sep_id is not None:
             queryset = queryset.filter(sep_id__contains=sep_id)
         if last_name is not None:
-            queryset = queryset.filter(last_name__icontains=last_name)
+            queryset = queryset.filter(last_name__istartswith=last_name)
         if last_four_ssn is not None:
             queryset = queryset.filter(last_four_ssn=last_four_ssn)
         if dob is not None:
@@ -31,6 +33,9 @@ class ParticipantViewSet(ModelViewSet):
         if pp_id is not None:
             queryset = queryset.filter(pp_id=pp_id)
         if maiden_name is not None:
-            queryset = queryset.filter(maiden_name__icontains=maiden_name)
+            queryset = queryset.filter(maiden_name__istartswith=maiden_name)
+        if not queryset.exists():
+            content = {'Whoopsie.':'No participant found.'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
 
         return queryset
