f071fcde5f4797d2ef5d8566333bc4506d33ec19
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2020-10-19 00:07:16 -0400

    Update SEP form validation

--- frontend/src/validation/index.js
+++ frontend/src/validation/index.js
@@ -26,46 +26,32 @@ const searchSchema = Yup.object().shape(
 const SEPSearchErrorMessage = "Please enter a value in at least one field"
 const SEPSearchSchema = Yup.object().shape(
   {
-    sep_id: Yup.string().when(
-      ["last_name", "date_of_birth", "mothers_last_name"],
-      {
-        is: (lastName, dateOfBirth, MothersLastName) =>
-          !lastName && !dateOfBirth && MothersLastName,
-        then: Yup.string().required(SEPSearchErrorMessage),
-      }
-    ),
-    last_name: Yup.string().when(
-      ["sep_id", "date_of_birth", "mothers_last_name"],
-      {
-        is: (sepId, dateOfBirth, MothersLastName) =>
-          !sepId && !dateOfBirth && MothersLastName,
-        then: Yup.string().required(SEPSearchErrorMessage),
-      }
-    ),
-    date_of_birth: Yup.string().when(
-      ["last_name", "sep_id", "mothers_last_name"],
-      {
-        is: (lastName, sepId, MothersLastName) =>
-          !lastName && !sepId && MothersLastName,
-        then: Yup.string().required(SEPSearchErrorMessage),
-      }
-    ),
-    mothers_last_name: Yup.string().when(
-      ["last_name", "date_of_birth", "sep_id"],
-      {
-        is: (lastName, dateOfBirth, sepId) =>
-          !lastName && !dateOfBirth && !sepId,
-        then: Yup.string().required(SEPSearchErrorMessage),
-      }
-    ),
+    sep_id: Yup.string().when(["last_name", "date_of_birth", "maiden_name"], {
+      is: (lastName, dateOfBirth, maidenName) =>
+        !lastName && !dateOfBirth && !maidenName,
+      then: Yup.string().required(SEPSearchErrorMessage),
+    }),
+    last_name: Yup.string().when(["sep_id", "date_of_birth", "maiden_name"], {
+      is: (sepId, dateOfBirth, maidenName) =>
+        !sepId && !dateOfBirth && !maidenName,
+      then: Yup.string().required(SEPSearchErrorMessage),
+    }),
+    date_of_birth: Yup.string().when(["last_name", "sep_id", "maiden_name"], {
+      is: (lastName, sepId, maidenName) => !lastName && !sepId && !maidenName,
+      then: Yup.string().required(SEPSearchErrorMessage),
+    }),
+    maiden_name: Yup.string().when(["last_name", "date_of_birth", "sep_id"], {
+      is: (lastName, dateOfBirth, sepId) => !lastName && !dateOfBirth && !sepId,
+      then: Yup.string().required(SEPSearchErrorMessage),
+    }),
   },
   [
     ["last_name", "date_of_birth"],
     ["last_name", "sep_id"],
-    ["mothers_last_name", "date_of_birth"],
+    ["maiden_name", "date_of_birth"],
     ["sep_id", "date_of_birth"],
-    ["sep_id", "mothers_last_name"],
-    ["last_name", "mothers_last_name"],
+    ["sep_id", "maiden_name"],
+    ["last_name", "maiden_name"],
   ]
 )
 
