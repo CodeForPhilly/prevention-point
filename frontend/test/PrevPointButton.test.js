b58223ebc25e2c1fbfba99420e098b0975b04fb6
JackRyan1989 <jack.jackryan@gmail.com>
2021-01-08 05:38:37 -0500

    Made text into variable.

--- frontend/test/PrevPointButton.test.js
+++ frontend/test/PrevPointButton.test.js
@@ -9,6 +9,7 @@ import PrevPointButton from "../src/components/PrevPointButton"
 // 3. onclick function
 describe("<PrevPointButton />", () => {
   const mockOnClick = jest.fn()
+  const text = "Search"
   let props
 
   beforeEach(() => {
@@ -22,31 +23,31 @@ describe("<PrevPointButton />", () => {
   })
   it("should render a PrevPointButton component", () => {
     const buttonWrapper = render(
-      <PrevPointButton {...props}>Search</PrevPointButton>
+      <PrevPointButton {...props}>{text}</PrevPointButton>
     )
     expect(buttonWrapper).toMatchSnapshot()
   })
 
   it("should render a PrevPointButton component with Search text", () => {
     const buttonWrapper = render(
-      <PrevPointButton {...props}>Search</PrevPointButton>
+      <PrevPointButton {...props}>{text}</PrevPointButton>
     )
     const button = buttonWrapper.queryByText("Search")
     expect(button).toHaveTextContent("Search")
   })
 
   it("Should have a button type of submit", () => {
-    const button = shallow(<PrevPointButton {...props}>Search</PrevPointButton>)
+    const button = shallow(<PrevPointButton {...props}>{text}</PrevPointButton>)
     expect(button.props().type).toBe("submit")
   })
 
   it("should have disabled set to false", () => {
-    const button = shallow(<PrevPointButton {...props}>Search</PrevPointButton>)
+    const button = shallow(<PrevPointButton {...props}>{text}</PrevPointButton>)
     expect(button.props().disabled).toBe(false)
   })
 
   it("should have working onclick function", () => {
-    const button = shallow(<PrevPointButton {...props}>Search</PrevPointButton>)
+    const button = shallow(<PrevPointButton {...props}>{text}</PrevPointButton>)
     button.simulate("click")
     expect(mockOnClick).toHaveBeenCalledTimes(1)
   })
